4681b114e4776710b9b5f42794e74ed8f0700c78
Marcel Dijkstra <marcel.acqui@gmail.com>
2014-04-30 19:24:53 +0000

    process float32

--- apps/server/server.m
+++ apps/server/server.m
@@ -1,27 +1,27 @@
 clear all; close all; clc;
-d=[];
+A=[];
 t = tcpip('130.89.20.144',4646);
 fopen(t);
 while(1)
   while (t.bytesavailable==0)
   end;
-  A = fread(t,t.bytesavailable);
-  A = fliplr(reshape(A,4,length(A)/4)');
-  a = (((A(:,1)*256)+A(:,2))*256+A(:,3))*256+A(:,4);
-  a = (a-8388608)./16777215.*8.192;
-  a = reshape(a,2,length(a)/2)';
-  d = [d; a];
+  A = [A; fread(t,t.bytesavailable/4,'float32');
+  B = reshape(A,[2 length(A)/2])';
 
-  figure(1); newplot;
-  plot(d(:,1)-d(1,1),'r-'); hold on;
-  plot(d(:,2)-d(1,2),'b-');
+  if (length(B)>100)
+    C = (B(:,1)-mean(B(1:100,1)))/std(B(1:100,1));
+    D = (B(:,1)-mean(B(1:100,2)))/std(B(1:100,2));
 
-  if(mod(length(d),100)==0)
+    figure(1);
+    hold off;
+    plot(C);
+    hold on;
+    plot(D,'r-');
+    axis tight;
+    
     figure(2);
-    subplot(2,1,1);
-    hist(d(:,1),100);
-    subplot(2,1,2);
-    hist(d(:,2),100);    
+    plot(D-C);
+    axis tight;
   end;
 end;
 fclose(t);
